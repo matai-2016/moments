eba9bc3d5daa685900cbfc86b68eef27f6905ff3
EDA <eda@mac-mini-1.local>
2017-03-24 11:47:49 +1300

    add reference to link moments table to people and images tables

--- migrations/20170324103752_moments.js
+++ migrations/20170324103752_moments.js
@@ -1,9 +1,9 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('moments', function (table) {
     table.increments('id').primary()
-    table.string('sender_id')
-    table.string('recipient_id')
-    table.string('image_id')
+    table.string('sender_id').references('people.id')
+    table.string('recipient_id').references('people.id')
+    table.string('image_id').references('images.id')
   })
 }
 
