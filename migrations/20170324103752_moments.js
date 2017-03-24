263749a1048fd5ece92acf874284424d07882324
EDA <eda@mac-mini-3.local>
2017-03-24 16:15:20 +1300

    fix type of foreign keys

--- migrations/20170324103752_moments.js
+++ migrations/20170324103752_moments.js
@@ -13,9 +13,9 @@ exports.up = function (knex, Promise) {
   ]).then(function () {
     return knex.schema.createTableIfNotExists('moments', function (table) {
       table.increments('id').primary()
-      table.string('sender_id').references('people.id')
-      table.string('recipient_id').references('people.id')
-      table.string('image_id').references('images.id')
+      table.integer('sender_id').references('people.id')
+      table.integer('recipient_id').references('people.id')
+      table.integer('image_id').references('images.id')
     })
   })
 }
