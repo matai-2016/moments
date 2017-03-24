f2c01354d4d8a2c241f509c0753d3c3be04c4b1e
EDA <eda@mac-mini-3.local>
2017-03-24 16:24:41 +1300

    make ids numeric

--- seeds/3-moments.js
+++ seeds/3-moments.js
@@ -2,9 +2,9 @@ exports.seed = function (knex, Promise) {
   return knex('moments').del()
     .then(function () {
       return knex('moments').insert([
-        {id: 1, sender_id: '8', recipient_id: '7', image_id: '8'},
-        {id: 2, sender_id: '4', recipient_id: '2', image_id: '4'},
-        {id: 3, sender_id: '6', recipient_id: '7', image_id: '3'}
+        {id: 1, sender_id: 8, recipient_id: 7, image_id: 8},
+        {id: 2, sender_id: 4, recipient_id: 2, image_id: 4},
+        {id: 3, sender_id: 6, recipient_id: 7, image_id: 3}
       ])
     })
 }
