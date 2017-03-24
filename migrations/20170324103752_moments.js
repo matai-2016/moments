d3bd96ee75edbb7cbca3f762d2b8d4940b419331
EDA <eda@mac-mini-3.local>
2017-03-24 16:08:20 +1300

    consolidated migratations

--- migrations/20170324103752_moments.js
+++ migrations/20170324103752_moments.js
@@ -1,12 +1,29 @@
 exports.up = function (knex, Promise) {
-  return knex.schema.createTableIfNotExists('moments', function (table) {
-    table.increments('id').primary()
-    table.string('sender_id').references('people.id')
-    table.string('recipient_id').references('people.id')
-    table.string('image_id').references('images.id')
+  return Promise.all([
+    knex.schema.createTableIfNotExists('people', function (table) {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('profile_image')
+    }),
+    knex.schema.createTableIfNotExists('images', function (table) {
+      table.increments('id').primary()
+      table.string('description')
+      table.string('url')
+    })
+  ]).then(function () {
+    return knex.schema.createTableIfNotExists('moments', function (table) {
+      table.increments('id').primary()
+      table.string('sender_id').references('people.id')
+      table.string('recipient_id').references('people.id')
+      table.string('image_id').references('images.id')
+    })
   })
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('moments')
+  return Promise.all([
+    knex.schema.dropTableIfExists('people'),
+    knex.schema.dropTableIfExists('images'),
+    knex.schema.dropTableIfExists('moments')
+  ])
 }
